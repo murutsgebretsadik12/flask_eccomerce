0d6fdd485126ecffa5190e28e990f0d069aed530
murutsmenge <murutsmenge21@gmail.com>
2024-03-28 12:13:15 +0000

    specify database URL

---
+++ config.py
@@ -0,0 +1,6 @@
+
+SQLALCHEMY_DATABASE_URI="postgresql://flaskprojecteccomerce_y7i7_user:yEQygoEEAi8M4FwK0uGu6iH9JsfGRG5y@dpg-co2l2rgl6cac73bpoo5g-a.frankfurt-postgres.render.com/flaskprojecteccomerce_y7i7"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Flask app configuration
+SECRET_KEY = 'secrete123456'
