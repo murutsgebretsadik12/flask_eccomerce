217c2127da9fd206aaceea6c6458b6c7ddfa1dc7
murutsmenge <murutsmenge21@gmail.com>
2024-04-01 10:43:43 +0100

    update model.py

--- model.py
+++ model.py
@@ -4,5 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Define Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False) 
+    image_data = db.Column(db.LargeBinary, nullable=True)
+    image_filename = db.Column(db.String(255), nullable=False)  
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+     
+
+
+
 def init_db(app: Flask):
     db.init_app(app)
